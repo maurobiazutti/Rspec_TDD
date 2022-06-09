89acba56b38b5793ca0b04888c65333476ce3f2f
maurobiazutti <mauro_biazutti@hotmail.com>
2022-06-08 21:47:05 -0300

    Initial commit

---
+++ lib/algoritimos_f01.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "algoritimos_f01/version"
+
+module AlgoritimosF01
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/algoritimos_f01/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AlgoritimosF01
+  VERSION = "0.1.0"
+end
