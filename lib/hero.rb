cae574d2d6cc64f7f9c938905601751b9dc74fa5
mauro <mauro_biazutti@hotmail.com>
2024-03-20 17:43:15 -0300

    Refatoração

--- lib/hero.rb
+++
@@ -1,8 +0,0 @@
-class Hero
-  attr_accessor :weapon
-
-  def initialize
-    @weapon = 'sword'
-  end
-end
-
--- lib/primeiros_testes_rspec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "primeiros_teste_rspec/version"
-
-module Primeiros_testes_rspec
-  class Error < StandardError; end
-  # Your code goes here...
-end
